bf5e203db7f5003bab2567b155ba100e26051e5e
hiramatsutaku <hiramatsutaku@gmail.com>
2020-07-01 18:03:55 +0900

    Add project

---
+++ src/components/ErrorBoundary/index.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+
+type Props = Record<string, unknown>;
+type State = {
+  hasError: boolean;
+};
+
+export class ErrorBoundary extends React.Component<Props, State> {
+  private constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  public static getDerivedStateFromError(): State {
+    return { hasError: true };
+  }
+
+  public componentDidCatch(error: Error, info: React.ErrorInfo): void {
+    // eslint-disable-next-line no-console
+    console.error('ErrorBoundary caught an error', error, info);
+  }
+
+  public render(): React.ReactNode {
+    const { hasError } = this.state;
+    if (hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+    const { children } = this.props;
+    return children;
+  }
+}
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './App';
+import { ErrorBoundary } from './components/ErrorBoundary';
+
+ReactDOM.render(
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>,
+  document.getElementById('app'),
+);
